6ca184472f6fbece238c931330e870b12892df1f
Charles Tuskan <tuskancj@gmail.com>
2019-03-06 21:25:40 -0500

    Initial Commit

---
+++ LASERS ARUITests/LASERS_ARUITests.swift
@@ -0,0 +1,34 @@
+//
+//  LASERS_ARUITests.swift
+//  LASERS ARUITests
+//
+//  Created by Charles Tuskan on 3/6/19.
+//  Copyright © 2019 Tuskan Technology, LLC. All rights reserved.
+//
+
+import XCTest
+
+class LASERS_ARUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
